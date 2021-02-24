afb7dc06889b10ca48583023ad250b637e5b0c8f
Peter Lau <superpeterlau@outlook.com>
2021-02-24 19:15:39 +0800

    push more

--- os/touros/0x00/print.asm
+++ os/touros/0x00/print.asm
@@ -1,29 +1,29 @@
 print:
     pusha
 
-start:
+print_start:
     mov al, [bx]
     cmp al, 0
-    je end
+    je print_end
 
     mov ah, 0x0e
-    init 0x10
+    int 0x10
 
     add bx, 1
-    jmp start
+    jmp print_start
 
-end:
+print_end:
     popa
     ret
 
 print_nl: ; 打印空行
-  pusha
+    pusha
 
-  mov ah, 0x0e
-  mov a1, 0x0a ; \n = 0x0a 0x0d
-  int 0x10
-  mov a1, 0x0d
-  int 0x10
+    mov ah, 0x0e
+    mov al, 0x0a ; \n = 0x0a 0x0d
+    int 0x10
+    mov al, 0x0d
+    int 0x10
 
-  popa
-  ret
+    popa
+    ret
