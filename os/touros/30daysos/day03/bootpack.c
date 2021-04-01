1eea9202dcb1940e9eb1f4552d6e4ca440a5f0c2
Peter Lau <superpeterlau@outlook.com>
2021-04-01 08:04:42 +0800

    30days_os: day03 harib00i: load C files

---
+++ os/touros/30daysos/day03/bootpack.c
@@ -0,0 +1,15 @@
+/*
+ * bootpack.c
+ * Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+/* #include "bootpack.h" */
+
+
+void HariMain(void)
+{
+fin:
+  goto fin;
+}
