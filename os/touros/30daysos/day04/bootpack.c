02e4347de68933981fe4d1905ebbb4539cae9579
Peter Lau <superpeterlau@outlook.com>
2021-04-09 00:13:29 +0800

    30days_os: day04 harib01d: c pointer 1

--- os/touros/30daysos/day04/bootpack.c
+++ os/touros/30daysos/day04/bootpack.c
@@ -15,14 +15,17 @@ void HariMain(void)
 {
   int i; 
   char *p;
+  p = (char *) 0xa0000;
 
   // mov byte [i], 15
   // 15 是全部颜色的第十五种颜色，即纯白色
-  for (i = 0xa0000; i <= 0xaffff; i++) {
+  // for (i = 0xa0000; i <= 0xaffff; i++) {
+  for (i = 0; i <= 0xffff; i++) {
     // write_mem8(i, i & 0x0f);
     // p = i;
-    p = (char *) i;
-    *p = i & 0x0f;
+    /* p = (char *) i; */
+    /* *p = i & 0x0f; */
+    *(p + i) = i & 0x0f;
   }
 
   for (;;) {
