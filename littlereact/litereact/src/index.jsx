d7ee13b10ca94a2114728569f22a8aa9236d376a
Peter Lau <superpeterlau@outlook.com>
2021-06-13 17:12:47 +0800

    litereact: step1

--- littlereact/litereact/src/index.jsx
+++ littlereact/litereact/src/index.jsx
@@ -1,16 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const TEXT_ELEMENT = 'TEXT_ELEMENT'  // Symbol
+
 function createElement(type, props, ...children) {
 	return {
 		type, 
 		props: {
 			...props,
-			children
+			children: children.map(child => 
+				typeof child === 'object'
+				? child 
+				: createTextElement(child)
+			)
 		}
 	}
 }
+
+function createTextElement(text) {
+	return {
+		type: TEXT_ELEMENT,
+		props: {
+			nodeValue: text,
+			children: []
+		}
+	}
+}
+
+const Liteact = {
+	createElement
+}
+
+const element = Liteact.createElement(
+	"div",
+	{id: "foo"},
+	Liteact.createElement("a", null, "bar"),
+	Liteact.createElement("b")
+)
+
+console.log(element)
+
+// const old = (
+// 	<div>Works</div>
+// )
+// console.log(old)
+
+/** @jsx Liteact.createElement */
+const elementJSX = (
+	<div id="foo">
+		<a>bar</a>
+	</div>
+)
+
 ReactDOM.render(
-	<div>Works</div>,
+	// <div>Works</div>,
+	element,
 	document.getElementById('root')
 )
