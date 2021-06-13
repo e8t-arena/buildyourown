e64dbef975d080d3c0fa60b55c368b5f2a68d47d
Peter Lau <superpeterlau@outlook.com>
2021-06-13 16:58:24 +0800

    litereact: own createElement

--- littlereact/litereact/src/index.jsx
+++ littlereact/litereact/src/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function createElement(type, props, ...children) {
+	return {
+		type, 
+		props: {
+			...props,
+			children
+		}
+	}
+}
 ReactDOM.render(
 	<div>Works</div>,
 	document.getElementById('root')
